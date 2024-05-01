74ee9adefcc5db69acfbe07c211514d8d4506367
Victor Fuentes <vmfuentes64@gmail.com>
2024-05-01 18:14:04 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:auxolotl/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = import ./lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      nixPackages = forAllSystems (system:
+        (import nixpkgs { inherit system; })
+      );
+
+      # auxPackages = forAllSystems (system:
+      #   (import ./. { inherit system; })
+      # );
+
+      # To test, run nix build .#tests.x86_64-linux.release
+      tests = forAllSystems (system:
+        ({
+          systems = import ./lib/tests/systems.nix;
+          release = import ./lib/tests/release.nix { pkgs = self.nixPackages.${system}; };
+        })
+      );
+    };
+}
